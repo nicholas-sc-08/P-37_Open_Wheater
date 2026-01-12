ed6394f5e324dc7863d885f6aca4fc8173dfd4da
nicholas-sc-08 <nicholassc.2008@gmail.com>
2026-01-12 15:49:18 -0300

    feat: inicial weather get working

--- src/main/java/br/com/ow/Open/Wheater/service/WeatherService.java
+++ src/main/java/br/com/ow/Open/Wheater/service/WeatherService.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import br.com.ow.Open.Wheater.dto.WeatherDTO;
+import br.com.ow.Open.Wheater.model.Weather;
 import br.com.ow.Open.Wheater.repository.WeatherRepo;
 
 @Service
@@ -12,16 +14,27 @@ public class WeatherService {
     
     @Autowired
     private WeatherRepo weatherRepo;
+    private RestTemplate restTemplate;
 
     @Value("${openweather.api.key}")
     private String apiKey;
 
-    public Object getByNameWeather(String name) {
+    public Weather getByNameWeather(String name) {
+        
         String url = "https://api.openweathermap.org/data/2.5/weather?q=" + name + "&appid=" + apiKey + "&units=metric&lang=pt-br";
         
-        RestTemplate restTemplate = new RestTemplate();
-        Object weather = restTemplate.getForObject(url, Object.class);
-        
-        return weather;
+        restTemplate = new RestTemplate();
+        WeatherDTO weather = restTemplate.getForObject(url, WeatherDTO.class);
+
+        Weather res = new Weather();
+        res.setName(weather.getName());
+        res.setMain(weather.getWeather().get(0).getMain());
+        res.setDescription(weather.getWeather().get(0).getDescription());
+        res.setIcon(weather.getWeather().get(0).getIcon());
+        res.setTemp(weather.getMain().getTemp());
+        res.setHumidity(weather.getMain().getHumidity());
+        res.setWindSpeed(weather.getWind().getSpeed());
+
+        return weatherRepo.save(res);
     } 
 }
